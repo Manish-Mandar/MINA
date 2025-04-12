d6fd6e68a76949e6d7cf0c4dc2012d7f784b601c
Manish-Mandar <manishmandar321@gmail.com>
2025-04-12 11:58:44 +0545

    hero page update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -75,7 +75,7 @@ const Home = () => {
         
         {/* Wave Divider */}
         <div className="absolute bottom-0 left-0 right-0 -mb-1">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="w-full">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -50 1440 320" className="w-full">
             <path fill="#ffffff" fillOpacity="1" d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,122.7C672,117,768,139,864,138.7C960,139,1056,117,1152,117.3C1248,117,1344,139,1392,149.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           </svg>
         </div>
